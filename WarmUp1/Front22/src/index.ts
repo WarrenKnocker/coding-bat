da58d7a55e5ba779c0e576555e488bdd19b5f626
WarrenKnocker <warren.knocker@gmail.com>
2023-07-31 13:18:08 +0200

    add front22

---
+++ WarmUp1/Front22/src/index.ts
@@ -0,0 +1,21 @@
+/* 
+
+
+Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". 
+If the string length is less than 2, use whatever chars are there.
+
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+
+*/
+
+export function front22(strParameter: string): string {
+  let charArray: string[] = strParameter.split("");
+  let TempChar = strParameter.slice(0, 2);
+
+  let newString = TempChar + strParameter + TempChar;
+
+  return newString;
+}
---
+++ WarmUp1/Front22/test/index.test.ts
@@ -0,0 +1,30 @@
+import { assert } from "chai";
+import { front22 } from "../src/index";
+
+/* 
+Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, 
+so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+
+*/
+
+describe("Front22", () => {
+  it("Should return 'kikittenki' if given the string parameter 'kitten' ", () => {
+    const result = front22("kitten");
+    assert.equal(result, "kikittenki");
+  });
+
+  it("Should return 'HaHaHa' if given the string parameter 'Ha' ", () => {
+    const result = front22("Ha");
+    assert.equal(result, "HaHaHa");
+  });
+
+  it("Should return 'ababcab' if given the string parameter 'abc' ", () => {
+    const result = front22("abc");
+    assert.equal(result, "ababcab");
+  });
+});
