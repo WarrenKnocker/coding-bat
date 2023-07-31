f09bb61ddec3c5634dc09d7a256c77d2cd29e5a6
WarrenKnocker <warren.knocker@gmail.com>
2023-07-31 10:41:19 +0200

    add notString

---
+++ WarmUp1/notString/src/index.ts
@@ -0,0 +1,22 @@
+/*
+
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+
+*/
+
+export function notString(strParameter: string): string {
+  let strArray: string[] = strParameter.split(" ");
+  let result: string;
+
+  if (strArray[0] !== "not") {
+    result = "not " + strParameter;
+    return result;
+  } else {
+    return strParameter;
+  }
+}
