0bc8473339cb265e97aa61636c4e0bc9fc433302
WarrenKnocker <warren.knocker@gmail.com>
2023-08-16 14:31:35 +0200

    updated test

--- Functional-1/doubling/test/index.test.ts
+++ Functional-1/doubling/test/index.test.ts
@@ -1,5 +1,6 @@
-import { assert, should } from "chai";
+import { assert } from "chai";
 import { doubling } from "../src/index";
+import { describe } from "mocha";
 
 /*
 Given a list of integers, return a list where each integer is multiplied by 2.
@@ -11,17 +12,17 @@ doubling([]) → []
 */
 
 describe("doubling", () => {
-  it("Should return a doubled array: [2, 4, 6] from the given array [1, 2, 3]", () => {
+  it("Should return a doubled array from the given array [1, 2, 3]", () => {
     const result = doubling([1, 2, 3]);
     assert.equal(result, [2, 4, 6]);
   });
 
-  it("Should return a doubled array: [12, 16, 12, 16, -2] from the given array [6, 8, 6, 8, -1]]", () => {
+  it("Should return a doubled from the given array [6, 8, 6, 8, -1]]", () => {
     const result = doubling([6, 8, 6, 8, -1]);
     assert.equal(result, [12, 16, 12, 16, -2]);
   });
 
-  it("Should return a doubled array: [] from the given array []", () => {
+  it("Should return an empty array from the given array []", () => {
     const result = doubling([]);
     assert.equal(result, []);
   });
