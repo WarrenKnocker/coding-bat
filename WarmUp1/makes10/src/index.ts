41a35576ae929394365447368932c4f59956b671
WarrenKnocker <warren.knocker@gmail.com>
2023-08-16 14:08:00 +0200

    simplified function

--- WarmUp1/makes10/src/index.ts
+++ WarmUp1/makes10/src/index.ts
@@ -1,10 +1,5 @@
-export function makes10(firstValue : number, secondValue : number) : Boolean {
-
-    if ((firstValue == 10 || secondValue == 10) || (firstValue + secondValue == 10)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+export function makes10(firstValue: number, secondValue: number): Boolean {
+  return (
+    firstValue == 10 || secondValue == 10 || firstValue + secondValue == 10
+  );
 }
-    
