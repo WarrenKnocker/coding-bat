4e3f624c7fb57ef6285d9e5c5883b6a0a1f338a4
WarrenKnocker <warren.knocker@gmail.com>
2023-08-25 17:13:08 +0200

    add arrayCount9

---
+++ WarmUp2/arrayCount9/src/index.ts
@@ -0,0 +1,19 @@
+/*
+Given an array of ints, return the number of 9's in the array.
+
+
+arrayCount9([1, 2, 9]) → 1
+arrayCount9([1, 9, 9]) → 2
+arrayCount9([1, 9, 9, 3, 9]) → 3
+*/
+
+export function arrayCount9(numArray: number[]): number {
+  let counter = 0;
+
+  for (let index = 0; index < numArray.length; index++) {
+    if (numArray[index] == 9) {
+      counter++;
+    }
+  }
+  return counter;
+}
