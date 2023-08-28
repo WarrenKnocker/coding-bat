10f2004359e1530546660315f58cadb0acdf30fe
WarrenKnocker <warren.knocker@gmail.com>
2023-08-28 15:26:38 +0200

    add stringYak

---
+++ WarmUp2/stringYak/src/index.ts
@@ -0,0 +1,15 @@
+/*
+
+Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+
+stringYak("yakpak") → "pak"
+stringYak("pakyak") → "pak"
+stringYak("yak123ya") → "123ya"
+*/
+
+export function stringYak(strInput: string): string {
+  let newString = strInput.replace("yak", "");
+
+  return newString;
+}
